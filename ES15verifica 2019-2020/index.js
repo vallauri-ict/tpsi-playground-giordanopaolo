9c797c0bb88a2867063a109cd23e0ce13bde79fd
unknown <p.giordano.1320@vallauri.edu>
2020-12-15 16:05:01 +0100

    impletazione es 15

---
+++ ES15verifica 2019-2020/index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+ $(document).ready(function(){
+
+	
+ });
+
+
+ 
+// Una semplice funzione per aggiungere uno 0 davanti ad un numero < 10
+function pad(number) {
+     return (number < 10 ? '0' : '') + number;
+}
